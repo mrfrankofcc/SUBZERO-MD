1779c8444ee13a5663f30bb2312ae065bc8de1c9
mrrmagnet <rethabileletswalo0@gmail.com>
2025-07-19 18:54:20 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID:SUBZERO-MD;;;zFl0gAKA#8h2m4EkuyHKbrMpZTaDOkgVbxr50bB0n2uDo9hdRwd8 "", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
