5e9a3dcf0baabee9473cd15e802a9e4ba7e8919f
Dhre-2 <jamon8537@gmail.com>
2025-07-25 20:11:25 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;iN0DhIQY#ktAp6l9Xx9YqFCEVo3eD1xCp0NB4QCpespVvYhIKqcs", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "2348031504740", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
