8255fe8b7ef5a79c4174866f5360fd1beaae7077
Ousmanekonate77 <154003625+Ousmanekonate77@users.noreply.github.com>
2025-08-06 07:39:32 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "SUBZERO-MD;;;LRF0kSZQ#GRRvhUp5u6711AjkiFE7Lb_d7jsDY3pNj4r01NlNS4g" // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;rZlGTSKC#FhqYXeoFgBFwMGxQctX_MoDuT1L-4w8mVuZ3meNmjtE", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "22383267744", // Put Owner Number Here 
+  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
 // CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
