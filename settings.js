530b404961daef6fb21ff32d31c753a26b622bf6
Letuka-deep <umletuka@gmail.com>
2025-08-18 22:42:29 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "", // SUBZERO-MD;;;PNdmkRqB#FkTzLqdwS0I24RCoAJrzT8PucWrVHTj8jDdfbWIFyDY "*"
+  OWNER_NUMBER: "26658572867", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
