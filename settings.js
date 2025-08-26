7155052f118d0e0e6baacdeb61ab6b952d4a96ba
Choi2025Stack <millionichoi@gmail.com>
2025-08-26 21:18:25 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO~1d8c3d074b4109ffb4397c78b292dd23aca5270b", 
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
