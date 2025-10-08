3860b210e085a4595d1b6afe0866d91387174fe6
1234Chrismuia <159400852+1234Chrismuia@users.noreply.github.com>
2025-10-08 18:25:59 +0300

    Update settings.js for jesphertech

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;OM8hmKDJ#CULVUa__jH0uV-zbvD38VeepWuUgmMhfe8ys8Jw555I",
+  OWNER_NUMBER: "+254710318560", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
