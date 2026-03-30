31da380b009822404ed2bf742a01be2d907fc932
Karl Al <marxmuronzi@gmail.com>
2026-03-30 19:06:56 +0200

    Update settings.js for Mr_Joshs_Bot

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263777965084", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "Ice~0Y3bXz",
+  OWNER_NUMBER: "+263713448887", 
+  PREFIX: ",",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY KARL AI MARX ( MARX MURONZI 🇿🇼 )
