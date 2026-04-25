822f521020955727df1c946661af40ccd15055d8
Karl Al <marxmuronzi@gmail.com>
2026-04-25 03:34:12 +0200

    Update settings.js for Karl-Al-v11

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263777965084", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "Ice~0UVdOm",
+  OWNER_NUMBER: "263773834309", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY KARL AI MARX ( MARX MURONZI 🇿🇼 )
